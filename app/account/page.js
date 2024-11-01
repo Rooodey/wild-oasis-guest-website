72f4a403cb6698423225889be360927b7ac10e17
Rudolf, Henrik (SE-A/32) <henrik.rudolf@volkswagen.de>
2024-11-01 16:12:36 +0100

    basics

---
+++ app/account/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Account",
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>Account</h1>
+    </>
+  );
+}
---
+++ app/cabins/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Cabins",
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>Cabins</h1>
+    </>
+  );
+}
